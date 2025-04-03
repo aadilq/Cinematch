7657581892f1399015a51918fca635287f56305d
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-04-03 16:30:56 -0400

    Create Search results page with query parameter handling

---
+++ frontend/src/pages/SearchResults.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import {useSearchParams} from 'react-router-dom';
+import SearchBar from '../components/layout/Searchbar';
+import movieGrid from '../components/movies/MovieGrid'; 
+
+
+
+const SearchResults = () => {
+    const searchParams = useSearchParams();
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchResults
